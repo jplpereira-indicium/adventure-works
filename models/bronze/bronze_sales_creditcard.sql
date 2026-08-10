8f3d54b6a5372c7e586e28424d0c08740b50d063
João Pedro Pereira <joao.lima@indicium.ai>
2026-08-10 13:49:11 +0000

    Adicionando o modelo bronze_sales_creditcard

---
+++ models/bronze/bronze_sales_creditcard.sql
@@ -0,0 +1,12 @@
+{{ config(materialized = 'table') }}
+
+with source_data as (
+
+    select
+        * 
+    from {{ source('adventure_works', 'sales_creditcard') }}
+
+)
+
+select *
+from source_data

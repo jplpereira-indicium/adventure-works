9a843ec436281b702e6f43be9196776ecae01856
jplpereira-indicium <joao.lima@indicium.ai>
2026-08-12 16:21:41 -0300

    Renomeando e convertendo as colunas do arquivo stg_sales_reason

--- models/staging/sales/stg_sales_reason.sql
+++ models/staging/sales/stg_sales_reason.sql
@@ -3,10 +3,21 @@
 with source_data as (
 
     select
-        * 
+        *
     from {{ source('adventure_works', 'sales_salesreason') }}
 
 )
 
+, renamed as (
+
+    select
+        cast(salesreasonid as int) as pk_sales_reason
+        ,cast(name as string) as name
+        ,cast(reasontype as string) as reason_type
+        ,cast(modifieddate as date) as modified_date
+    from source_data
+
+)
+
 select *
-from source_data
+from renamed

3d71ee9aa93185c21bdbf99dc361ef253e432d02
jplpereira-indicium <joao.lima@indicium.ai>
2026-08-12 16:22:36 -0300

    Renomeando e convertendo as colunas do arquivo stg_person_country_region

--- models/staging/person/stg_person_country_region.sql
+++ models/staging/person/stg_person_country_region.sql
@@ -3,10 +3,20 @@
 with source_data as (
 
     select
-        * 
+        *
     from {{ source('adventure_works', 'person_countryregion') }}
 
 )
 
+, renamed as (
+
+    select
+        cast(countryregioncode as string) as pk_country_region
+        ,cast(name as string) as name
+        ,cast(modifieddate as date) as modified_date
+    from source_data
+
+)
+
 select *
-from source_data
+from renamed

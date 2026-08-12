de9af4c68b2c78a5d4fd2c848b624c03032d7218
jplpereira-indicium <joao.lima@indicium.ai>
2026-08-12 16:22:10 -0300

    Renomeando e convertendo as colunas do arquivo stg_sales_customer

--- models/staging/sales/stg_sales_customer.sql
+++ models/staging/sales/stg_sales_customer.sql
@@ -3,10 +3,23 @@
 with source_data as (
 
     select
-        * 
+        *
     from {{ source('adventure_works', 'sales_customer') }}
 
 )
 
+, renamed as (
+
+    select
+        cast(customerid as int) as pk_customer
+        ,cast(personid as int) as fk_person
+        ,cast(storeid as int) as fk_store
+        ,cast(territoryid as int) as fk_territory
+        ,rowguid
+        ,cast(modifieddate as date) as modified_date
+    from source_data
+
+)
+
 select *
-from source_data
+from renamed

bad50d7a87f3ac066638858f35b342af0b2a23ef
jplpereira-indicium <joao.lima@indicium.ai>
2026-08-12 16:21:34 -0300

    Renomeando e convertendo as colunas do arquivo stg_sales_creditcard

--- models/staging/sales/stg_sales_creditcard.sql
+++ models/staging/sales/stg_sales_creditcard.sql
@@ -3,10 +3,23 @@
 with source_data as (
 
     select
-        * 
+        *
     from {{ source('adventure_works', 'sales_creditcard') }}
 
 )
 
+, renamed as (
+
+    select
+        cast(creditcardid as int) as pk_credit_card
+        ,cast(cardtype as string) as card_type
+        ,cast(cardnumber as string) as card_number
+        ,cast(expmonth as int) as expiration_month
+        ,cast(expyear as int) as expiration_year
+        ,cast(modifieddate as date) as modified_date
+    from source_data
+
+)
+
 select *
-from source_data
+from renamed

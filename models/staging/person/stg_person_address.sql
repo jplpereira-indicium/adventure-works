ec8241a3bd35a52ac689748def06cbc3886ed9e3
jplpereira-indicium <joao.lima@indicium.ai>
2026-08-12 16:22:22 -0300

    Renomeando e convertendo as colunas do arquivo stg_person_address

--- models/staging/person/stg_person_address.sql
+++ models/staging/person/stg_person_address.sql
@@ -3,10 +3,26 @@
 with source_data as (
 
     select
-        * 
+        *
     from {{ source('adventure_works', 'person_address') }}
 
 )
 
+, renamed as (
+
+    select
+        cast(addressid as int) as pk_address
+        ,cast(addressline1 as string) as address_line_1
+        ,cast(addressline2 as string) as address_line_2
+        ,cast(city as string) as city
+        ,cast(stateprovinceid as int) as fk_state_province
+        ,cast(postalcode as string) as postal_code
+        ,spatiallocation as spatial_location
+        ,rowguid
+        ,cast(modifieddate as date) as modified_date
+    from source_data
+
+)
+
 select *
-from source_data
+from renamed

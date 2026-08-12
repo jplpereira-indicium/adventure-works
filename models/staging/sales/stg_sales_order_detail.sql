007dde6fb91cb26baffa10ed54fda4a7963d0c14
jplpereira-indicium <joao.lima@indicium.ai>
2026-08-12 16:22:03 -0300

    Renomeando e convertendo as colunas do arquivo stg_sales_order_detail

--- models/staging/sales/stg_sales_order_detail.sql
+++ models/staging/sales/stg_sales_order_detail.sql
@@ -3,10 +3,27 @@
 with source_data as (
 
     select
-        * 
+        *
     from {{ source('adventure_works', 'sales_salesorderdetail') }}
 
 )
 
+, renamed as (
+
+    select
+        cast(salesorderid as int) as fk_sales_order
+        ,cast(salesorderdetailid as int) as pk_sales_order_detail
+        ,cast(carriertrackingnumber as string) as carrier_tracking_number
+        ,cast(orderqty as int) as order_quantity
+        ,cast(productid as int) as fk_product
+        ,cast(specialofferid as int) as fk_special_offer
+        ,cast(unitprice as decimal(15, 2)) as unit_price
+        ,cast(unitpricediscount as decimal(15, 2)) as unit_price_discount
+        ,rowguid
+        ,cast(modifieddate as date) as modified_date
+    from source_data
+
+)
+
 select *
-from source_data
+from renamed

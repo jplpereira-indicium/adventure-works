da38abb50931768522332fb42aba9b3a583caf0b
jplpereira-indicium <joao.lima@indicium.ai>
2026-08-12 16:22:45 -0300

    Renomeando e convertendo as colunas do arquivo stg_person_state_province

--- models/staging/person/stg_person_state_province.sql
+++ models/staging/person/stg_person_state_province.sql
@@ -3,10 +3,25 @@
 with source_data as (
 
     select
-        * 
+        *
     from {{ source('adventure_works', 'person_stateprovince') }}
 
 )
 
+, renamed as (
+
+    select
+        cast(stateprovinceid as int) as pk_state_province
+        ,cast(stateprovincecode as string) as state_province_code
+        ,cast(countryregioncode as string) as fk_country_region
+        ,isonlystateprovinceflag as is_only_state_province
+        ,cast(name as string) as name
+        ,cast(territoryid as int) as fk_territory
+        ,rowguid
+        ,cast(modifieddate as date) as modified_date
+    from source_data
+
+)
+
 select *
-from source_data
+from renamed

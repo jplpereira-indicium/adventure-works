9505250b5af3fc656b6a712db79cfbf8f096d6f8
jplpereira-indicium <joao.lima@indicium.ai>
2026-08-12 16:23:11 -0300

    Renomeando e convertendo as colunas do arquivo stg_production_product_subcategory

--- models/staging/production/stg_production_product_subcategory.sql
+++ models/staging/production/stg_production_product_subcategory.sql
@@ -3,10 +3,22 @@
 with source_data as (
 
     select
-        * 
+        *
     from {{ source('adventure_works', 'production_productsubcategory') }}
 
 )
 
+, renamed as (
+
+    select
+        cast(productsubcategoryid as int) as pk_product_subcategory
+        ,cast(productcategoryid as int) as fk_product_category
+        ,cast(name as string) as name
+        ,rowguid
+        ,cast(modifieddate as date) as modified_date
+    from source_data
+
+)
+
 select *
-from source_data
+from renamed
